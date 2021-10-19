a9bafee31c3d435ccb9939bcdf82b023d73259e6
noaeitan <noaeitan2@gmail.com>
2021-10-19 02:39:03 -0700

    3 last functions

--- anomaly_detection_util.cpp
+++ anomaly_detection_util.cpp
@@ -1,19 +1,36 @@
 /*
  * anomaly_detection_util.cpp
  *
- * Author: write your ID and name here
+ * Author: Noa Eitan 316222777, Coral Kuta 208649186
  */
 
-#include <math.h>
+#include <cmath>
 #include "anomaly_detection_util.h"
+#include <exception>
 
-float avg(float* x, int size){
-	return 0;
+float* get_X_from_points(Point** points, int size);
+float* get_Y_from_points(Point** points, int size);
+
+// return the average of the values that in the array
+float avg(const float* x, int size){
+    // size is 0 or less. return 0.
+    if (size < 1) {
+        return 0;
+    }
+    float sum = 0;
+
+    // sum the values from the array
+    for (int i = 0; i < size; i++) {
+        sum += x[i];
+    }
+
+    // return the average
+    return (sum / (float) size);
 }
 
 // returns the variance of X and Y
 float var(float* x, int size){
-	return 0;
+	return size;
 }
 
 // returns the covariance of X and Y
@@ -29,18 +46,63 @@ float pearson(float* x, float* y, int size){
 
 // performs a linear regression and returns the line equation
 Line linear_reg(Point** points, int size){
+    float a, b;
 
-	return Line(0,0);
+    // initial arrays of x and y values, of the points
+    float *x = get_X_from_points(points, size);
+    float *y = get_Y_from_points(points, size);
+
+    // calculate a
+    a = cov(x, y, size) / var(x, size);
+
+    // calculate b
+    float average_x = avg(x, size);
+    float average_y = avg(y, size);
+    b = average_y - (a * average_x);
+	return {a, b};
+}
+
+// return array of x values of points, from array of points
+float* get_X_from_points(Point** points, int size){
+    auto* arr = new float[size];
+
+    // scan the array of points and initial new array with the x values of the points
+    for (int i = 0; i < size; i++) {
+        arr[i] = points[i]->get_x();
+    }
+    return arr;
+}
+
+// return array of y values of points, from array of points
+float* get_Y_from_points(Point** points, int size){
+    auto* arr = new float[size];
+
+    // scan the array of points and initial new array with the x values of the points
+    for (int i = 0; i < size; i++) {
+        arr[i] = points[i]->get_y();
+    }
+    return arr;
 }
 
 // returns the deviation between point p and the line equation of the points
 float dev(Point p,Point** points, int size){
-	return 0;
+    // save the line from the array of points
+	Line line = linear_reg(points, size);
+
+    // return the deviation between point p and the line
+    return dev(p, line);
 }
 
 // returns the deviation between point p and the line
 float dev(Point p,Line l){
-	return 0;
+    // save the y value of the point
+    float y = p.get_y();
+
+    // get the real value of y (f(x))
+    float fx = l.f(p.get_x());
+
+    // return the absolute distance between the y value of the point and the y value of the line
+    return fabsf(y - fx);
 }
 
 
--- anomaly_detection_util.h
+++ anomaly_detection_util.h
@@ -2,10 +2,10 @@
 #ifndef ANOMALYDETECTORUTIL_H_
 #define ANOMALYDETECTORUTIL_H_
 
+// return the average of the values that in the array
+float avg(const float* x, int size);
 
-float avg(float* x, int size);
-
-// returns the variance of X and Y
+// returns the variance of X
 float var(float* x, int size);
 
 // returns the covariance of X and Y
@@ -16,19 +16,31 @@ float cov(float* x,  float* y, int size);
 float pearson(float* x, float* y, int size);
 
 class Line{
-public:
 	float a,b;
+public:
 	Line():a(0),b(0){}
 	Line(float a, float b):a(a),b(b){}
+
+    // return f(x) from the liner equation of the line
 	float f(float x){
 		return a*x+b;
 	}
 };
 
 class Point{
-public:
 	float x,y;
+public:
 	Point(float x, float y):x(x),y(y){}
+
+    //return the x field
+    float get_x(){
+        return x;
+    }
+
+    //return the y field
+    float get_y(){
+        return y;
+    }
 };
 
 // performs a linear regression and returns the line equation
